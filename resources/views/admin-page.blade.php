d65ba0b14d8631a74919f3c99f4ed689ff45d5de
miyu suzuki <bu.m6s.pq@gmail.com>
2024-03-01 14:30:32 +0900

    管理ページ更新

--- resources/views/admin-page.blade.php
+++ resources/views/admin-page.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div class="manager-page__content">
-        <p>管理者</p>
+        <p>管理者ページ</p>
         <div class="store-representative_register">
             <a href="{{ route('register.show') }}" class="store-representative_register__button">店舗代表者アカウント<br>新規作成</a>
         </div>
