2f19813f992cc3c6052a664642bc71e991aa30a2
miyu suzuki <bu.m6s.pq@gmail.com>
2024-03-01 15:03:00 +0900

    管理ページ更新

--- resources/views/admin-page.blade.php
+++ resources/views/admin-page.blade.php
@@ -10,8 +10,5 @@
         <div class="store-representative_register">
             <a href="{{ route('register.show') }}" class="store-representative_register__button">店舗代表者アカウント<br>新規作成</a>
         </div>
-        <div class="store-representative_mailform">
-            <a href="{{ route('mailform.show') }}" class="store-representative_mailform__button">メールフォーム</a>
-        </div>
     </div>
 @endsection
